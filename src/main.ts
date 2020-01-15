a8d7e8c1cddc9ee7cbb1519023283ee85ff96f9e
Смирнова Александра Игоревна <smirnova.a14@wildberries.ru>
2020-01-15 13:28:58 +0300

    compiled moduleA component with errors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import Store from './services/store'
 import App from './App.vue'
 import system from './modules/system/module'
 import siteModule from './modules/site/module'
-import moduleA from './modules/moduleA/module'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import wrap from '@vue/web-component-wrapper'
+import ModuleSite from './modules/site/ModuleSite.vue'
+import ModuleA from './modules/moduleA/ModuleA.vue'
 
 Vue.use(BootstrapVue)
 Vue.use(ElementUI)
@@ -19,6 +21,10 @@ Store.registerModule('system', system.store)
 Router.addRoutes(system.routes)
 Store.dispatch('system/initializeModule', siteModule)
 // Store.dispatch('system/initializeModule', moduleA)
+
+// Initialize web-components
+const ModuleAElement = wrap(Vue, ModuleA)
+window.customElements.define('module-a-element', ModuleAElement)
 
 new Vue({
   template: '<App/>',
