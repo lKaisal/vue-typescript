c9f3db21896d45472735917063f995626bd5207f
Kaisa <alexandra.i.smirnova@gmail.com>
2020-04-06 18:25:00 +0300

    upd SearchApp - remove library, added native string search by all fields

--- src/components/SearchApp.vue
+++ src/components/SearchApp.vue
@@ -29,6 +29,9 @@ export default class SearchApp extends Vue {
   searchText: string = null // EL-INPUT
   search = null // JsSearch instance
   JsSearch!: any
+  searchedList: Object[] = null
+  listInitial: Object[] = null
+
   get uniqueField() { return this.fields[this.uniqueFieldIndex].field }
 
   @Watch('list', {deep:true, immediate: true})
@@ -36,47 +39,55 @@ export default class SearchApp extends Vue {
     console.log('list upd ' + this.list.length + ' ' + new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds())
   }
   created() {
-    console.log('search created ' + new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds())
-    this.initSearch()
+    this.searchedList = [...this.list]
+    this.listInitial = [...this.list]
+    // console.log('search created ' + new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds())
+    // this.initSearch()
   }
   async initSearch(field?: SearchField['field']) {
-    this.JsSearch = await import ('js-search')
-    console.log('search inited ' + new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds())
-    this.search = new this.JsSearch.Search(this.uniqueField)
-    this.search.indexStrategy = new this.JsSearch.AllSubstringsIndexStrategy()
+    // this.JsSearch = await import ('js-search')
+    // console.log('search inited ' + new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds())
+    // this.search = new this.JsSearch.Search(this.uniqueField)
+    // this.search.indexStrategy = new this.JsSearch.AllSubstringsIndexStrategy()
 
-    if (field) this.search.addIndex(field)
-    else {
-      for (const field of this.fields) {
-        this.search.addIndex(field.field)
-      }
-    }
+    // if (field) this.search.addIndex(field)
+    // else {
+    //   for (const field of this.fields) {
+    //     this.search.addIndex(field.field)
+    //   }
+    // }
 
-    this.search.addDocuments([...this.list])
-    this.onSearchTextChange()
+    // this.search.addDocuments([...this.list])
+    // this.onSearchTextChange()
   }
   async onSearchTextChange() {
     if (!this.searchText) {
       this.$emit('searchFinished')
-      if (this.activeField) {
-        this.activeField = null
-        this.search = null
-        await this.$nextTick()
-        this.initSearch()
-      }
+      await this.$nextTick()
+      this.searchedList = this.listInitial
     } else {
-      const searchRes = this.search.search(this.searchText.toString())
-      this.$emit('searchProgress', searchRes)
+      const textFormatted = this.searchText.toString().trim().toLowerCase()
+
+      this.searchedList = this.listInitial.filter(supplier => {
+        return this.fields.some((field, index) => {
+          const supplierValue = supplier[field.field]
+          const supplierValueFormatted = supplierValue.toString().trim().toLowerCase()
+          return supplierValueFormatted.includes(textFormatted)
+        })
+      })
+
+      this.$emit('searchProgress', this.searchedList)
     }
   }
   async onActiveFieldChange() {
-    // !!! reset list (or it will filter only list left after previous filter)
-    this.$emit('searchFinished')
-    await this.$nextTick()
-    this.search = null
+    console.log(this.activeField)
+    // // !!! reset list (or it will filter only list left after previous filter)
+    // this.$emit('searchFinished')
+    // await this.$nextTick()
+    // this.search = null
 
-    // init JsSearch with (or without) activeField
-    this.initSearch(this.activeField)
+    // // init JsSearch with (or without) activeField
+    // this.initSearch(this.activeField)
   }
 }
 </script>
