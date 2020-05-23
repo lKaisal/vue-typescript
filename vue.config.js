4cfd6c6e10c4533abac2321261bd8cfa70eefda5
Kaisa <alexandra.i.smirnova@gmail.com>
2020-05-23 18:38:17 +0300

    savepoint

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,2 @@
 module.exports = {
-  devServer: {
-    // port: 8080,
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:8080/',
-        changeOrigin: true,
-      },
-      '/login': {
-        target: 'http://localhost:8080/',
-        changeOrigin: true,
-      },
-      '/refresh': {
-        target: 'http://localhost:8080/',
-        changeOrigin: true,
-      }
-    }
-  },
 }
